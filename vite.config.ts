4bb1c5c3f5370ccbcc72ea5e6cb1c6555e9e1dfa
matb85 <mateuszbis85@gmail.com>
2026-03-28 12:28:36 +0100

    chore: add vite-plugin-mkcert

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   server: {
     port: 5170,
   },
